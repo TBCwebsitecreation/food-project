6140f9bf3c94db72309e6cb2467b424e7b93df4e
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2021-02-24 12:58:28 -0700

    adding media queries

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// top menu
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('change')
+  })
+})
+
+
+
+
+
+
 // 1.select all the icons and store them in a variable called icons
 const icons = document.querySelectorAll('.section-1-icons i')
 
